a7f297759801c3a65592dc7ba39371114f2ac32a
nktnvrvskj <141362044+nktnvrvskj@users.noreply.github.com>
2026-07-10 17:11:19 +0300

    Update LauncherActivity.java
    Signed-off-by: nktnvrvskj <141362044+nktnvrvskj@users.noreply.github.com>

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/LauncherActivity.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/LauncherActivity.java
@@ -1,7 +1,6 @@
 package net.kdt.pojavlaunch;
 
 import static android.content.res.Configuration.ORIENTATION_PORTRAIT;
-import static net.kdt.pojavlaunch.Tools.hasNoOnlineProfileDialog;
 
 import android.Manifest;
 import android.app.NotificationManager;
@@ -155,23 +154,7 @@ public class LauncherActivity extends BaseActivity {
         }
         String normalizedVersionId = AsyncMinecraftDownloader.normalizeVersionId(prof.lastVersionId);
         JMinecraftVersionList.Version mcVersion = AsyncMinecraftDownloader.getListedVersion(normalizedVersionId);
-
-        // Do not load when is a modded version or older than minecraft 1.3 on demo account
-        if (mAccountSpinner.getSelectedAccount().isDemo()) {
-            boolean isOlderThan13 = true;
-
-            if (mcVersion != null) {
-                try {
-                    isOlderThan13 = DateUtils.dateBefore(DateUtils.parseReleaseDate(mcVersion.releaseTime), 2012, 6, 22);
-                } catch (ParseException ignored) {}
-            }
-
-            if (isOlderThan13) {
-                hasNoOnlineProfileDialog(this, getString(R.string.global_error), getString(R.string.demo_versions_supported));
-                return false;
-            }
-        }
-
+        
         new MinecraftDownloader().start(
                 this,
                 mcVersion,
