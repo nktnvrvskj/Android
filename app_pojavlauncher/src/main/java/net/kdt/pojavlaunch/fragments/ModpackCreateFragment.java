7e7316fd17a654893adf03283fe7118f49313a4f
nktnvrvskj <141362044+nktnvrvskj@users.noreply.github.com>
2026-07-10 17:33:20 +0300

    Update ModpackCreateFragment.java
    Signed-off-by: nktnvrvskj <141362044+nktnvrvskj@users.noreply.github.com>

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/ModpackCreateFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/ModpackCreateFragment.java
@@ -41,10 +41,7 @@ public class ModpackCreateFragment extends Fragment {
     }
 
     private void tryInstall(Class<? extends Fragment> fragmentClass, String tag){
-        if(!hasOnlineProfile()){
-            hasNoOnlineProfileDialog(requireActivity());
-        } else {
-            Tools.swapFragment(requireActivity(), fragmentClass, tag, null);
-        }
+        Tools.swapFragment(requireActivity(), fragmentClass, tag, null);
+
     }
 }
